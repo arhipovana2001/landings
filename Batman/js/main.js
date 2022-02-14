7d86f02a972e44fb7512f72ee4fcf65c0eaa5ea4
Nastya Arkhipova <55373084+arhipovana2001@users.noreply.github.com>
2022-02-14 20:21:14 +0700

    Batman landing is done

---
+++ Batman/js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.main-slider', {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.main-slider-arrow',
+  },
+});
